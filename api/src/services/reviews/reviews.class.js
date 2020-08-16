64750f03de5c6d44e99c451325dd2f2a8419cec4
quannt <simpleplan.vn@gmail.com>
2020-08-16 23:26:57 +0900

    Set status when creating new review

--- api/src/services/reviews/reviews.class.js
+++ api/src/services/reviews/reviews.class.js
@@ -1,9 +1,13 @@
-const { Service } = require('feathers-nedb');
+const { Service } = require("feathers-nedb");
 
+const ReviewStatus = {
+  NotStarted: "not_started",
+  InProgress: "in_progress",
+  Submitted: "submitted",
+};
 exports.Reviews = class Reviews extends Service {
-  create (data, params) {
-
-    const { reviewer_id, reviewee_id, content, status } = data;
+  create(data, params) {
+    const { reviewer_id, reviewee_id, content } = data;
     const created = new Date();
     const last_updated = new Date();
     // The complete review
@@ -11,15 +15,15 @@ exports.Reviews = class Reviews extends Service {
       reviewer_id,
       reviewee_id,
       content,
-      status,
+      status: ReviewStatus.NotStarted,
       created,
-      last_updated
+      last_updated,
     };
 
     // Call the original `create` method with existing `params` and new data
     return super.create(reviewData, params);
   }
-  update (id, data, params) {
+  update(id, data, params) {
     data.last_updated = new Date();
     return super.patch(id, data, params);
   }
